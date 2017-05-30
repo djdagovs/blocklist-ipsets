8fcc3ee908f532df843c2860b992c72a93a685dc
Costa Tsaousis <costa@tsaousis.gr>
2017-05-30 08:37:07 +0100

    Tue May 30 07:37:07 UTC 2017 update

--- set_file_timestamps.sh
+++ set_file_timestamps.sh
@@ -77,7 +77,7 @@
 [ -f 'blocklist_de_sip.ipset' ] && /usr/bin/touch --date=@1496128229 'blocklist_de_sip.ipset'
 [ -f 'blocklist_de_ssh.ipset' ] && /usr/bin/touch --date=@1496129417 'blocklist_de_ssh.ipset'
 [ -f 'blocklist_de_strongips.ipset' ] && /usr/bin/touch --date=@1496126209 'blocklist_de_strongips.ipset'
-[ -f 'blocklist_net_ua.ipset' ] && /usr/bin/touch --date=@1496129102 'blocklist_net_ua.ipset'
+[ -f 'blocklist_net_ua.ipset' ] && /usr/bin/touch --date=@1496129404 'blocklist_net_ua.ipset'
 [ -f 'blueliv_crimeserver_last.ipset' ] && /usr/bin/touch --date=@1488085980 'blueliv_crimeserver_last.ipset'
 [ -f 'blueliv_crimeserver_last_1d.ipset' ] && /usr/bin/touch --date=@1488085980 'blueliv_crimeserver_last_1d.ipset'
 [ -f 'blueliv_crimeserver_last_2d.ipset' ] && /usr/bin/touch --date=@1488085980 'blueliv_crimeserver_last_2d.ipset'
